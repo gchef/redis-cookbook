75256a8a77e5f046eeb62e78c99d6301591a6bda
Gerhard Lazu <gerhard@lazu.co.uk>
2010-08-06 16:00:30 +0100

    Redis 1.2.6 cookbook Couldn't find it anywhere online, so took a few hours to create one

---
+++ attributes/redis.rb
@@ -0,0 +1,22 @@
+default[:redis][:version]   = "1.2.6"
+default[:redis][:source]    = "http://redis.googlecode.com/files/redis-#{redis[:version]}.tar.gz"
+default[:redis][:checksum]  = "c71aef0b3f31acb66353d86ba57dd321b541043f"
+default[:redis][:bins]      = %w(redis-benchmark redis-cli redis-server redis-stat)
+
+default[:redis][:dir]       = "/opt/redis-#{redis[:version]}"
+default[:redis][:datadir]   = "/data/redis"
+default[:redis][:logfile]   = "/var/log/redis.log"
+default[:redis][:pidfile]   = "/var/run/redis.pid"
+
+default[:redis][:port]        = 6379
+default[:redis][:timeout]     = 300
+default[:redis][:databases]   = 16
+default[:redis][:max_memory]  = 256
+default[:redis][:snapshots]   = {
+  900 => 1,
+  300 => 10,
+  60  => 10000
+}
+default[:redis][:dbfilename]   = "redis_state.rdb"
+default[:redis][:bind_address]= "0.0.0.0"
+default[:redis][:loglevel]    = "notice"
---
+++ metadata.rb
@@ -0,0 +1,92 @@
+maintainer        "Paper Cavalier"
+maintainer_email  "code@papercavalier.com"
+license           "Apache 2.0"
+description       "Installs and configures redis"
+version           "0.0.1"
+
+recipe "redis::source", "Installs redis from source and adds init script"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
+
+%w{ build-essential }.each do |cb|
+  depends cb
+end
+
+attribute "redis/version",
+  :display_name => "Redis Version",
+  :description => "Which Redis version we're installing",
+  :default => "1.2.6"
+
+attribute "redis/source",
+  :display_name => "Redis source file",
+  :description => "Downloaded location for Redis"
+
+attribute "redis/checksum",
+  :display_name => "Redis source file checksum",
+  :description => "Will make sure the source file is the real deal"
+
+attribute "redis/bins",
+  :display_name => "Redis scripts",
+  :description => "After make, these are the scripts which will become available"
+
+attribute "redis/dir",
+  :display_name => "Redis installation path",
+  :description => "Redis will be installed here",
+  :default => "/opt/redis-1.2.6"
+
+attribute "redis/datadir",
+  :display_name => "Redis data store",
+  :description => "All Redis data will be stored here",
+  :default => "/data/redis"
+
+attribute "redis/logfile",
+  :display_name => "Redis log file",
+  :description => "Redis will log to this file",
+  :default => "/var/log/redis.log"
+
+attribute "redis/pidfile",
+  :display_name => "Redis PID file",
+  :description => "Path to Redis PID",
+  :default => "/var/run/redis.pid"
+
+attribute "redis/port",
+  :display_name => "Redis port",
+  :description => "Accept connections on the specified port",
+  :default => "6379"
+
+attribute "redis/timeout",
+  :display_name => "Redis timeout",
+  :description => "Close the connection after a client is idle for this many seconds (0 to disable)",
+  :default => "300"
+
+attribute "redis/databases",
+  :display_name => "Redis databases",
+  :description => "Number of databases",
+  :default => "16"
+
+attribute "redis/max_memory",
+  :display_name => "Redis max memory",
+  :description => "Don't use more memory than the specified amount of MBs",
+  :default => "256"
+
+attribute "redis/snapshots",
+  :display_name => "Redis snapshots",
+  :description => "Will save the DB if both the given number of seconds and the given number of write operations against the DB occurred.",
+  :default => { 900 => 1, 300 => 10, 60 => 10000 }
+
+attribute "redis/dbfilename",
+  :display_name => "Redis dbfilename",
+  :description => "The filename where to dump the DB",
+  :default => "redis_state.rdb"
+
+attribute "redis/bind_address",
+  :display_name => "Redis bind address",
+  :description => "Redis will listen on all IP addresses by default",
+  :default => "0.0.0.0"
+
+attribute "redis/loglevel",
+  :display_name => "Redis log level",
+  :description => "Sets server verbosity. Will be moderately verbose, what you want in production probably",
+  :default => "notice"
