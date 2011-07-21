a058334a23dcabf2f3a5f471dac1f51b96e87d49
Gerhard Lazu <gerhard@lazu.co.uk>
2011-07-21 11:05:21 +0100

    Fixes GH-4. Thanks Bruce.

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -19,7 +19,6 @@
 # limitations under the License.
 #
 
-# TODO: support more than one master
-node.set[:redis][:slaveof] = search(:node, 'recipes:redis\:\:master').first.ec2.local_hostname
+node.set[:redis][:slaveof] = search(:node, 'chef_environment:#{node.chef_environment} AND recipes:redis::master').first.first.ec2.local_hostname
 
 include_recipe "redis::source"
