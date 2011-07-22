23df94aca57dc90bea3733b9d086bb348f3f45d2
Gerhard Lazu <gerhard@lazu.co.uk>
2011-07-22 16:54:29 +0100

    Absolute path to template

--- recipes/init.rb
+++ recipes/init.rb
@@ -1,6 +1,7 @@
 case node[:redis][:init]
 when "init"
   template "/etc/init.d/redis" do
+    cookbook "redis"
     source "redis.sysv.erb"
     mode 0755
     backup false
@@ -8,6 +9,7 @@ when "init"
   end
 when "upstart"
   template "/etc/init/redis.conf" do
+    cookbook "redis"
     source "redis.upstart.erb"
     mode 0644
     backup false
