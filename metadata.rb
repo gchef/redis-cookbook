1ea8bc0831a556cbf0b407ecd976da16a3f8828a
Gerhard Lazu <gerhard@lazu.co.uk>
2011-07-27 14:47:20 +0100

    Copyright & chef notices

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ license           "Apache 2.0"
 description       "Installs and configures Redis 2.2.12"
 version           "2.2.12"
 
-recipe "redis::source", "Installs redis from source and adds init script"
+recipe "redis::source", "Installs redis from source"
+recipe "redis::master", "Installs redis from source and configures it as a master instance"
+recipe "redis::slave", "Installs redis from source and configures it as a slave instance"
 
 %w{ ubuntu debian }.each do |os|
   supports os
