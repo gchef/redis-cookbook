a1adb5af2dc64e179aeb21738dd0b8a127854839
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-06 00:12:28 +0100

    Those license notices are really annoying It's OSS, use it as you please

--- recipes/init.rb
+++ recipes/init.rb
@@ -1,24 +1,3 @@
-#
-# Cookbook Name:: redis
-# Recipe:: init
-#
-# Author:: Gerhard Lazu (<gerhard@lazu.co.uk>)
-#
-# Copyright 2011, Gerhard Lazu
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 case node[:redis][:init]
 when "init"
   template "/etc/init.d/redis" do
--- recipes/slave.rb
+++ recipes/slave.rb
@@ -1,24 +1,3 @@
-#
-# Cookbook Name:: redis
-# Recipe:: slave
-#
-# Author:: Gerhard Lazu (<gerhard@lazu.co.uk>)
-#
-# Copyright 2011, Gerhard Lazu
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 node.set[:redis][:slaveof] = search(:node, 'chef_environment:#{node.chef_environment} AND recipes:redis::master').first.first.ec2.local_hostname
 
 include_recipe "redis::source"
