1fcaab434ee639c7db30afbd63138bdfb6e3ec56
Gerhard Lazu <gerhard@lazu.co.uk>
2011-12-09 15:32:56 +0000

    Upgrade to reeds 2.4.4. More info in the README.

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,12 +1,11 @@
 include_recipe "build-essential"
 
 user "redis" do
-  comment "Redis Administrator"
   system true
-  shell "/bin/false"
+  shell "/bin/sh"
 end
 
-directory node[:redis][:datadir] do
+directory node.redis.datadir do
   owner "redis"
   group "redis"
   mode 0755
@@ -17,12 +16,12 @@ include_recipe "redis::init"
 
 service "redis" do
   supports :start => true, :stop => true, :restart => true
-  provider Chef::Provider::Service::Upstart if node[:redis][:init] == "upstart"
+  provider Chef::Provider::Service::Upstart if node.redis.init == "upstart"
 end
 
-# download and install a new redis version
+# download and install a new redis version if not running the latest
 #
-unless `redis-server -v 2> /dev/null`.include?(node[:redis][:version])
+unless `redis-server -v 2>/dev/null`.include?(node.redis.version)
 
   # stop if a previous redis version has been installed
   #
@@ -33,24 +32,26 @@ unless `redis-server -v 2> /dev/null`.include?(node[:redis][:version])
 
   # ensuring we have this directory
   #
-  directory "#{node[:redis][:basedir]}/src"
+  directory node.redis.srcdir
 
-  remote_file "#{node[:redis][:basedir]}/src/redis-#{node[:redis][:version]}.tar.gz" do
-    source node[:redis][:source]
-    checksum node[:redis][:checksum]
+  remote_file "#{node.redis.srcdir}/#{node.redis.dir}.tar.gz" do
+    source node.redis.source
+    checksum node.redis.checksum
     action :create_if_missing
   end
 
   bash "Compiling Redis v#{node[:redis][:version]} from source" do
-    cwd "#{node[:redis][:basedir]}/src"
+    cwd node.redis.srcdir
     code %{
-      tar zxf redis-#{node[:redis][:version]}.tar.gz
-      cd redis-#{node[:redis][:version]} && make && make install
+      if [ ! -d #{node.redis.dir} ]; then
+        tar zxf #{node.redis.dir}.tar.gz
+      fi
+      cd #{node.redis.dir} && make && make install
     }
   end
 end
 
-file node[:redis][:logfile] do
+file node.redis.logfile do
   owner "redis"
   group "redis"
   mode 0644
@@ -64,12 +65,14 @@ template node[:redis][:config] do
   group "redis"
   mode 0644
   backup false
+  notifies :restart, resources(:service => "redis"), :delayed
 end
 
 execute "echo 1 > /proc/sys/vm/overcommit_memory" do
   not_if "[ $(cat /proc/sys/vm/overcommit_memory) -eq 1 ]"
+  notifies :restart, resources(:service => "redis"), :delayed
 end
 
 service "redis" do
-  action [:enable, :restart]
+  action [:enable, :start]
 end
